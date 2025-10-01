4ab567eb389ef1781879e0f04b242f5cd1b03f2c
mmooraes <mmoraes.job@outlook.com>
2025-10-01 10:13:42 -0300

    add desafio005

---
+++ Mundo 1/PythonProject/PythonDesafios/desafio005.py
@@ -0,0 +1,7 @@
+#Faca um programa que leia um numero inteiro e 
+#mostre na tela o seu sucessor e o seu antecessor.
+
+n1 = int(input('Digite um numero: '))
+s = n1 + 1
+a = n1 - 1
+print('O numero ditado e {}, o seu sucessor e {} e o seu antecessor e {}.'.format(n1, s, a))
