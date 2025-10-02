63d78907f89e4cdc85cd3f73cacdcd4935df731b
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-02 11:29:13 -0300

    update_desafio003

--- Mundo 1/PythonProject/PythonDesafios/desafio009.py
+++ Mundo 1/PythonProject/PythonDesafios/desafio009.py
@@ -1,7 +1,7 @@
 #Faça um programa que leia um numero inteiro
 #qualquer e mostre na tela a sua tabuada
 
-n1 = int(input('Qual a tabuado que voce deseja? '))
+n1 = int(input('Escolha uma tabuada: '))
 a = n1*1
 b = n1*2
 c = n1*3
@@ -13,4 +13,4 @@ h = n1*8
 i = n1*9
 j = n1*10
 
-print('Voce escolheu a tabuada do {}\n{}x1= {}\n{}x2= {}\n{}x3= {}\n{}x4= {}\n{}x5= {}\n{}x6= {}\n{}x7= {}\n{}x8= {}\n{}x9= {}\n{}x10= {}'.format(n1, n1, a, n1, b, n1, c, n1, d, n1, e, n1, f, n1, g, n1, h, n1, i, n1, j))
+print('Você escolheu a tabuada do {}\n{}x1= {}\n{}x2= {}\n{}x3= {}\n{}x4= {}\n{}x5= {}\n{}x6= {}\n{}x7= {}\n{}x8= {}\n{}x9= {}\n{}x10= {}'.format(n1, n1, a, n1, b, n1, c, n1, d, n1, e, n1, f, n1, g, n1, h, n1, i, n1, j))
