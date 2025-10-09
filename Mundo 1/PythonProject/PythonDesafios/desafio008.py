a35b08051bbf6d4e8a9cb57ad3d4defdf54ca7c6
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-09 16:58:46 -0300

    correção_desafio008

--- Mundo 1/PythonProject/PythonDesafios/desafio008.py
+++ Mundo 1/PythonProject/PythonDesafios/desafio008.py
@@ -3,6 +3,6 @@
 
 n1 = float(input('Digite um valor em metros: '))
 c = n1 * 100
-mm = float(n1 * 1000)
+mm = n1 * 1000
 #print('O valor digitado: {:.2f}m! Sua conversão para centimetros: {:,.0f} cm, e em milimetros: {:_.0f} mm'.format(n1, c, mm).replace('_','.'))#
-print('O Valor difitado: {:.2f}m! Sua conversão para centimentros: {} cm e em milimetros {:.0f} mm '.format(n1, c, mm))
+print('O Valor difitado: {:.2f}m! Sua conversão para centimentros: {:.0f}cm e em milimetros {:.0f}mm '.format(n1, c, mm))
