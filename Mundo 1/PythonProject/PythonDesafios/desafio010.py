a071a9bb5f0dd6d4b5be151a9b13dbb5c4be6b37
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-09 17:32:42 -0300

    add_correcao_desafio010
    coloquei também as conversões de Euro e Peso Argentino

--- Mundo 1/PythonProject/PythonDesafios/desafio010.py
+++ Mundo 1/PythonProject/PythonDesafios/desafio010.py
@@ -1,9 +1,13 @@
 #Crie um programa que leia quanto dinheiro uma pessoal
 #tem na carteira e mostre quantos Dólares ela pode comprar
-#considere US$ 1,00 = R$ 3,27
+#considere US$ 1,00 = R$ 3,27 (use o dolar do dia)
 
-carteira = float(input('Digite o valor presente na carteira: '))
-dolar = (float(3.27))
-conversao = carteira/dolar
+carteira = float(input('Digite o valor presente na carteira R$'))
+dolar = carteira / 5.38
+euro = carteira / 6.22
+pesoArgentino = carteira / 0.0038
 
-print(f'Voce tem na carteira R$ {carteira:.2f}. Voce pode comprar US$ {conversao:.2f}')
+
+print(f'Voce tem na carteira R${carteira:.2f}. Voce pode comprar US${dolar:.2f}')
+print(f'Voce tem na carteira R${carteira:.2f}. Voce pode comprar €{euro:.2f}')
+print(f'Voce tem na carteira R${carteira:.2f}. Voce pode comprar ${pesoArgentino:.2f}')
