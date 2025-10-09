0aa0a090b3fbb8fa0669d9fff3e89db12499f4f8
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-09 17:15:08 -0300

    add_correcao_desafio009

--- Mundo 1/PythonProject/PythonDesafios/desafio009.py
+++ Mundo 1/PythonProject/PythonDesafios/desafio009.py
@@ -13,4 +13,8 @@ h = n1*8
 i = n1*9
 j = n1*10
 
-print('Você escolheu a tabuada do {}\n{}x1= {}\n{}x2= {}\n{}x3= {}\n{}x4= {}\n{}x5= {}\n{}x6= {}\n{}x7= {}\n{}x8= {}\n{}x9= {}\n{}x10= {}'.format(n1, n1, a, n1, b, n1, c, n1, d, n1, e, n1, f, n1, g, n1, h, n1, i, n1, j))
+
+print('Você escolheu a tabuada do {}'.format(n1))
+print('='*14)
+print('{} x 1 = {}\n{} x 2 = {}\n{} x 3 = {}\n{} x 4 = {}\n{} x 5 = {}\n{} x 6 = {}\n{} x 7 = {}\n{} x 8 = {}\n{} x 9 = {}\n{} x 10 = {}'.format(n1, a, n1, b, n1, c, n1, d, n1, e, n1, f, n1, g, n1, h, n1, i, n1, j))
+print('='*14)
