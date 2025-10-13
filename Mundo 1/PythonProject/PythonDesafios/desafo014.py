1985f16f754ff32912482c2aab17dab20db6619d
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-13 12:24:17 -0300

    add_desafio015
    desafio + correção

---
+++ Mundo 1/PythonProject/PythonDesafios/desafo014.py
@@ -0,0 +1,8 @@
+"""Aula 15:
+Escreva um programa que converta uma temperatura digitada em °C e converta para °F
+"""
+c = float(input('Informe a temperatura em °C: '))
+f = 9 * c / 5 + 32
+#nesse caso não precisamos utilizar o () por conta da ordem de precedecia 
+
+print(f'A tempertura de {c}°C corresponde a {f}°F!')
