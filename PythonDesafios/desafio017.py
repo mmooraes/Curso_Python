5ec27344d649ea0f0e1c231b6e2c55f5c369e43c
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-20 14:48:30 -0300

    add correção desafio 17
    aqui utilizei forma matemática e também uma biblioteca

--- PythonDesafios/desafio017.py
+++ PythonDesafios/desafio017.py
@@ -3,10 +3,19 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto
 adjacente de um tringulo retangulo, calcule e mostre o comprimento da hipotenusa
 """
 
+"""
+#AQUI USAMOS O METODO MATEMATICO 
 oposto = float(input('Digite o valor do cateto oposto: '))
 adjacente = float(input('Digite o valor do cateto adjascente: '))
-soma = (oposto**2 + adjacente**2)
-hipotenusa = soma**(1/2)
+hipotenusa = (oposto**2 + adjacente**2) ** (1/2)
 
-print(f'A soma do cateto oposto {oposto} + o adjacente {adjacente} resulta no comprimento da hipotenusa de {hipotenusa}')
+print(f'A soma do cateto oposto {oposto} + o adjacente {adjacente} resulta no comprimento da hipotenusa de {hipotenusa:.2f}')
+"""
 
+#AQUI VAMOS UTILIZAR IMPORTAÇÃO DE BIBLIOTECA
+import math
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca)
+
+print(f'A soma do cateto oposto {co} + o adjacente {ca} resulta no comprimento da hipotenusa de {hi:.2f}')
