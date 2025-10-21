2a0c17d4aada2ef55f2ec228ef7616b073518b97
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-21 12:38:15 -0300

    Desafio019
    Correção

--- PythonDesafios/desafio019.py
+++ PythonDesafios/desafio019.py
@@ -2,7 +2,7 @@
 UM PROFESSOR QUER SORTEAR UM DOS SEUS QUATRO ALUNOS PARA APAGAR O QUADRO. FAÇA UMA PROGRAMA 
 QUE LEIA O NOME DELES ESCREVENDO O NOME DOS ECOLHIDOS 
 """
-
+""""
 import random
 alunoUm = str(input('Aluno 01: '))
 alunoDois = str(input('Aluno 02: '))
@@ -12,5 +12,14 @@ sorteado = random.randint(1,4)
 
 print(f'Os aulunos são: \n 01- {alunoUm} \n 02- {alunoDois} \n 03- {alunoTres} \n 04- {alunoQuatro}')
 print(f'O Aluno sorteado é {sorteado}')
+"""
+import random
+alunoUm = str(input('Aluno 01: '))
+alunoDois = str(input('Aluno 02: '))
+alunoTres = str(input('Aluno 03: '))
+alunoQuatro = str(input('Aluno 04: '))
+lista = [alunoUm, alunoDois, alunoTres, alunoQuatro]
+escolhido = random.choice(lista)
 
-    
+print(f'Os aulunos são: \n 01- {alunoUm} \n 02- {alunoDois} \n 03- {alunoTres} \n 04- {alunoQuatro}')
+print(f'O Aluno sorteado é {escolhido}')
