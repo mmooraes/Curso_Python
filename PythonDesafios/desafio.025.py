ab04558025fd0ee54fb644ce3ffd612c3604c368
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-30 13:45:20 -0300

    update
    desafio 25 e 24

---
+++ PythonDesafios/desafio.025.py
@@ -0,0 +1,7 @@
+"""DESAFIO 25 - AULA 09
+CRIE UM PROGRAMA QUE LEIA O NOME DE UMA PESSOA E DIFA SE TEM
+"SILVA" NO NOME
+"""
+
+cidade = str(input('Digite o seu nome completo: '))
+print(f'{cidade}, tem "Silva" no nome?', "Silva" in cidade)
--- PythonDesafios/desafio024.py
+++ PythonDesafios/desafio024.py
@@ -3,5 +3,8 @@ CRIE UM PROGRAMA EM PY QUE LEIA O NOME DE UMA CIDADE E DIGA
 SE ELA COMEÇA OU NÃO COM O NOME SANTO
 """
 
-cidade = str(input('Digite o nome da uma cidade: '))
-print(f'a cidade {cidade} tem ou não "Santo" no nome?', "Santo" in cidade)
+cidade = str(input('Digite o nome da uma cidade: ')).strip()
+if cidade.find('Santo') == 0:
+    print(f'A cidade {cidade}, começa com Santo')
+else:
+    print(f'A cidade {cidade}, Não começa com Santo')
