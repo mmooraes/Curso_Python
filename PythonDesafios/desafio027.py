f61a1ce6180818ebd1a5b7b3c21dc5944cd00d1e
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-31 11:00:26 -0300

    update desafio027
    Correção

--- PythonDesafios/desafio027.py
+++ PythonDesafios/desafio027.py
@@ -2,7 +2,8 @@
 FAÇA UM PROGRAM EM PY QUE LEIA O NOME COMPLETO DE UMA PESSA,
 MOSTRANDO EM SEGUIDA O PRIMEIRO E O ULTIMO NOME SEPARADAMENTE
 """
-nome = str(input('Digite seu nome completo: ')).strip()
-print(f'O nome completo é {nome}')
-print('O primeiro nome é: ', nome.split()[0])
-print('O ultimo nome é: ', nome.rsplit())
+n = str(input('Digite seu nome completo: ')).strip()
+nome = n.split()
+print(f'O nome completo é {n}')
+print('Seu primeiro nome é: ', (nome[0]))
+print('O ultimo nome é: ', (nome[len(nome)-1]))
