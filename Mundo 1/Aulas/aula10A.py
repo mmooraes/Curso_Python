48468575a6befc917ca7a4afef6b17a825468a4a
Matheus Moraes <mmoraes.job@outlook.com>
2025-10-31 12:09:19 -0300

    update aula10A

---
+++ Mundo 1/Aulas/aula10A.py
@@ -0,0 +1,17 @@
+
+tempo = int(input('Quantos anos o seu carro tem? '))
+
+if tempo <= 3:
+    print('O Seu carro esta novo!')
+else:
+    print('O seu carro ja não é mais novo!')
+print('--FIM--')
+
+"""
+
+## MESMO CODIGO (VERSÃO SIMPLIFICADA)
+tempo = int(input('Quantos anos o seu carro tem? '))
+print('carro novo'if tempo <=3 else 'carro velho')
+print('--FIM--')
+
+"""
