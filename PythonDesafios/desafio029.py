558591ed7a03d1b2835f9d6f0844b6ee93d4ba61
Matheus Moraes <mmoraes.job@outlook.com>
2025-11-03 17:31:25 -0300

    Create desafio029.py

---
+++ PythonDesafios/desafio029.py
@@ -0,0 +1,15 @@
+""" DESAFIO 029 - AULA 10
+CRIE UM PROGRAMA QUE LEIA A VELOCIDADE DE UM CARRO.
+
+SE ELE ULTRAPASSAR 80Km/h, MOSTRE UMA MENSAGEM DIZENDO QUE ELE FOI MULTADO
+
+A MULTA VAI CUSTAR R$ 7,00 PO KM ACIMA DO LIMITE
+"""
+
+import math
+
+km = int(input('Digite a velocidade do veiculo: '))
+
+if km <= 80:
+    print('Veiculo esta dentro da velocidade permitida')
+else 80 // 10 % 10:
