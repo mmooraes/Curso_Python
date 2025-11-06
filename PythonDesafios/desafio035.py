b6aafc69db641d36eb75e608e631030ed34cc57c
mmooraes <mmoraes.job@outlook.com>
2025-11-05 22:20:46 -0300

    add desafio 035

--- PythonDesafios/desafio035.py
+++ PythonDesafios/desafio035.py
@@ -2,3 +2,12 @@
 DESENVOLVA UM PROGRAMA QUE LEIA O COMPRIMENTO DE TRES RETAS E DIGA AO USUARIO
 SE ELAS PODEM OU NAO FORMAR UM TRIANGULO
 """
+print('CALCULANDO UM TRIANGULO')
+a = int(input('Digite o valor A: '))
+b = int(input('Digite o valor B: '))
+c = int(input('Digite o valor C: '))
+
+if (a + b > c) and (a + c > b) and (b + c > a):
+    print(f'Parabens! Voce tem um triangulo')
+else:
+    print(f'Que pena! Voce nao tem um triangulo')
