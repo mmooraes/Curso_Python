03e0cc941fac7b1a454f35243feec17331fa9d9e
Matheus Moraes <mmoraes.job@outlook.com>
2025-11-10 11:33:19 -0300

    update desafio029
    Feito correção do FLOAT
    
    também feio com o método de (condição simples), utilizando somente o ig

--- PythonDesafios/desafio029.py
+++ PythonDesafios/desafio029.py
@@ -6,13 +6,21 @@ SE ELE ULTRAPASSAR 80Km/h, MOSTRE UMA MENSAGEM DIZENDO QUE ELE FOI MULTADO
 A MULTA VAI CUSTAR R$ 7,00 PO KM ACIMA DO LIMITE
 """
 
-import math
-
-km = int(input('Digite a velocidade do veiculo: '))
+"""MINHA LOGICA
+km = float(input('Digite a velocidade do veiculo: '))
 multa = (km - 80) * 7
 
 if km <= 80:
-    print('Veiculo esta dentro da velocidade permitida')
+    print('Voce esta dentro do limite. Tenha um bom dia!')
 else:
-    print(f'Voce ultrapassou o limite de velocidade e foi multado em R${multa:.2f}')
+    print(f'Voce ultrapassou o limite de velocidade. Deverá pagar uma multada de R${multa:.2f}')"""
+
+
+#LOGICA DO PROFESSOR - Utilizou a estrutura somente com IF (condição simples)
+km = float(input('Digite a velocidade do veiculo: '))
+if km > 80:
+    print('MULTADO! Você excedu o limite permitido que é de 80Km/h')
+    multa = (km - 80) * 7
+    print(f'Você deve pagar uma multa de R${multa:.2f}!')
+print('Tenha um bom dia! Dirija com segurança!')
 
