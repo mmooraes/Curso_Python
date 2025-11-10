1657f75133efabd8c9311232e9174b920269f6b1
Matheus Moraes <mmoraes.job@outlook.com>
2025-11-10 11:17:42 -0300

    update desafio 028
    Correção e melhoria
    foi colocado time
    e melhoria na sintaxe do cód.

--- PythonDesafios/desafio028.py
+++ PythonDesafios/desafio028.py
@@ -6,17 +6,20 @@ PELO COMPUTADOR
 O PROGRAMA DEVERÁ ESCREVER NA TELA SE O USUARIO VENCEU OU PERDEU
 """
 
-from random import choice
+from random import randint
+from time import sleep
 import emoji
+
 
 print('-=-' *20)
 print('  ' *10, 'JOGO DE ADVINHAÇÃO')
 print('-=-' *20)
-n1 = int(input('De 0 a 5. Qual número estou pensando?'))
+n1 = int(input('De 0 a 5. Qual número estou pensando? '))
 print('---' *20)
-n = [0, 1, 2, 3, 4, 5]
-n2 = choice(n)
-if n2 == n1:
+n = randint (0, 5) #Faz o computador pensar em um numero
+print('PROCESSANDO...')
+sleep(3) # da um tempo entre a resposta, dando a sensação que o computador esta analisando 
+if n == n1:
     print(emoji.emojize(':green_circle: - Parabens! Voce acertou!'))
 else:
-    print(emoji.emojize(f':red_circle: Voce errou! o meu número era {n2}'))
+    print(emoji.emojize(f':red_circle: Voce errou! o meu número era {n}'))
