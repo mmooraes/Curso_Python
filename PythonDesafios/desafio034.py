fdec8161a7ee7a3f0a385ddadc906d70ff756a68
Matheus Moraes <mmoraes.job@outlook.com>
2025-11-10 13:49:14 -0300

    update desafio034
    Adicionada outra logica de realizar o desafio

--- PythonDesafios/desafio034.py
+++ PythonDesafios/desafio034.py
@@ -6,12 +6,21 @@ PARA SALARIO SUPERIORES A R$1.250,00, CALCULE UM AUMENTO DE 10%
 
 PARA OS INFERIORES OU IGUAIS, O AUMENTO E DE 15%
 """
-
+"""" # Minha Logica
 salario = float(input('Digite o salario para receber o aumento: ').replace(",", "."))
 aumentoum = salario + (salario * 10 / 100)
-aumentdois = salario + (salario * 15/ 100)
+aumentdois = salario + (salario * 15 / 100)
 
 if salario > 1.250:
     print(f'Voce recebeu 10% de aumento: {aumentoum:.3f}')
 else:
-    print(f'Voce recebeu 15% de aumento: {aumentdois:.3f}')
+    print(f'Voce recebeu 15% de aumento: {aumentdois:.3f}')"""
+
+
+# Lógica do professro
+salario = float(input('Digite atual do salario R$ ').replace(",", "."))
+if salario <= 1250:
+    novo = salario + (salario * 15 / 100)
+else:
+    novo = salario + (salario * 10 / 100)
+print(f'Quem ganha R${salario:.2f} passa a ganhar R$ {novo:.2f} agora')
